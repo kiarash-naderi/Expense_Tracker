66050d702724104c171d5f7bbee86b52fd1b1c5b
kiarash-naderi <kiarashnaaderi@gmail.com>
2025-08-08 18:51:35 +0330

    update models structure

--- src/models/Expense.ts
+++ src/models/Expense.ts
@@ -1,11 +1,18 @@
 import { Group } from "./Group";
 import { User } from "./User";
 
+
+export interface Payer {
+    user: User, //kodom user
+    amount: number, // che ghadar
+}
+
+
 export interface Expense {
     id: string,
     amount: number;
     description: string;
-    paidBy: User;
+    paidBy: Payer[]; // list, chon chan nafar mitonan pardakht konan
     group: Group;
     date: Date;
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,11 @@
+import { Expense } from "./Expense";
+
 export interface User {
     id: string;
     name: string;
+    expenses: { expense: Expense; amountPaid: number }[]; // hame hazine haye user x
+    
+    /* expenses: Expense[] in eshtebahe chon saheme har user ro neshon nemide 
+    agar id 1 va 2 har do pardakht konan, har do kol hazine ro mibinan 
+    na faghat sahme khodeshoon */
 }
